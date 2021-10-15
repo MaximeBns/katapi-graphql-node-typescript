a723c03fcc879c0d3b536f9a4d7d6c6605a20d7e
Remydeme <remy.deme@octo.com>
2021-10-15 14:08:23 +0200

    try to fix the issue

--- src/configuration/database/createConntection.ts
+++ src/configuration/database/createConntection.ts
@@ -1,11 +1,12 @@
 import ProduitModel from "../../catalogue/infrastructure/adapter/models/produit.model";
 import {Connection, createConnection} from "typeorm";
+import * as sqlite from "expo-sqlite"
 
 export async function createSqliteConnection(): Promise<Connection> {
 	const createdConnection = await createConnection({
 		type: 'expo',
 		database: 'todos.db',
-		driver: require('expo-sqlite'),
+		driver: sqlite,
 		entities: [ProduitModel],
 		migrationsRun: false,
 		synchronize: false,
