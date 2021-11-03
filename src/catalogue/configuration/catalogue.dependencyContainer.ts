e622c49a780464e6483f7e620e4b465ca2724291
DEME Remy <Remydeme@users.noreply.github.com>
2021-11-03 11:56:33 +0100

    Fix/run (#3)
    * remove sqlite dependencies
    
    * remove typeorm dependencies
    
    Co-authored-by: Remydeme <remy.deme@octo.com>

--- src/catalogue/configuration/catalogue.dependencyContainer.ts
+++ src/catalogue/configuration/catalogue.dependencyContainer.ts
@@ -1,7 +1,6 @@
 import RecupererLeProduit from "../usecases/recupererLeProduit";
 import RecupererLesProduits from "../usecases/recupererLesProduits";
 import CreerUnProduit from "../usecases/creerUnProduit";
-import {Connection} from "typeorm";
 import ProduitAdapter from "../infrastructure/adapter/ProduitAdapter";
 
 
@@ -11,8 +10,8 @@ export type CatalogueDependencyContainer = {
 	creerUnProduit: CreerUnProduit,
 };
 
-export default function CreateCatalogueDependencyContainer(connection: Connection): CatalogueDependencyContainer {
-	const produitAdapter = new ProduitAdapter(connection)
+export default function CreateCatalogueDependencyContainer(): CatalogueDependencyContainer {
+	const produitAdapter = new ProduitAdapter()
 	const recupererLeProduitUseCase = new RecupererLeProduit(produitAdapter)
 	const recupererLesProduitsUseCase = new RecupererLesProduits(produitAdapter)
 	const creerUnProduit = new CreerUnProduit(produitAdapter)
--- src/server.ts
+++ src/server.ts
@@ -6,31 +6,28 @@ import cors from 'cors';
 import 'graphql-import-node';
 import * as typeDefs from './catalogue/application/rootSchema.graphql';
 import {makeExecutableSchema} from 'graphql-tools';
-import {createSqliteConnection} from "./configuration/database/createConnection";
 import {createServerDependenciesContainer} from "./configuration/serverDependencyContainer";
 import Resolver from "./catalogue/application/resolverMap";
 
-createSqliteConnection().then((connection) => {
-	const serverDependencyContainer = createServerDependenciesContainer(connection)
+const serverDependencyContainer = createServerDependenciesContainer()
 
-	const resolvers = new Resolver(serverDependencyContainer).getResolvers()
-	const schema = makeExecutableSchema({
-		typeDefs,
-		resolvers,
-	});
+const resolvers = new Resolver(serverDependencyContainer).getResolvers()
+const schema = makeExecutableSchema({
+	typeDefs,
+	resolvers,
+});
 
-	const app = express();
-	app.use('*', cors());
-	app.use(compression());
-	const httpServer = createServer(app);
+const app = express();
+app.use('*', cors());
+app.use(compression());
+const httpServer = createServer(app);
 
-	const server = new ApolloServer({
-		schema,
-	});
-	server.start().then(r => server.applyMiddleware({app, path: '/graphql'}));
+const server = new ApolloServer({
+	schema,
+});
+server.start().then(r => server.applyMiddleware({app, path: '/graphql'}));
 
-	httpServer.listen(
-		{port: 3000},
-		(): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
-})
+httpServer.listen(
+	{port: 3000},
+	(): void => console.log(`\n🚀      GraphQL is now running on http://localhost:3000/graphql`));
 
