466d5fe83fb13ba7f7a1410cb55d016e7990eb05
maxime.bienassis <maxime.bienassis@octo.com>
2021-11-23 19:27:04 +0100

    feat(Ajout code erreur): Ajout d'un code d'erreur sur la liste de produits

--- src/catalogue/domain/entities/produit.ts
+++ src/catalogue/domain/entities/produit.ts
@@ -15,6 +15,8 @@ export default class Produit {
 
     static creer(nom: string, prix: number, poids: number): Produit {
         if (nom.length < Produit.TAILLE_NOM_MINIMUM) throw new Error("Le nom du produit est trop court");
-        return new Produit("1", nom, prix, poids)
+      const randomId = Math.round(Math.random() *10).toString();
+
+      return new Produit(randomId, nom, prix, poids)
     }
 }
