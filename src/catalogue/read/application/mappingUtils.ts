dbc31aaf20b199c98bffe0757fc386c2677cab1a
Bastien Mourrat <bastien.mourrat@octo.com>
2022-01-12 08:19:00 +0100

    fix mapping resolver

---
+++ src/catalogue/read/application/mappingUtils.ts
@@ -0,0 +1,31 @@
+import {CommandeOutputApi, ElementCommandeOutputApi} from "../../application/commande/CommandeOutputApi";
+import Commande, {CommandeElement} from "../domain/entities/commande/commande";
+import Produit from "../domain/entities/produit";
+
+export function toCommandeOutputApi(commande: Commande): CommandeOutputApi {
+    return {
+        __typename: 'Commande',
+        id: commande.id,
+        statut: commande.statut,
+        elements: commande.elements.map(toElementCommandeOuputApi),
+        fraisDePort: commande.fraisDePort,
+        montantTotal: commande.montantTotal,
+        poids: commande.poids
+    }
+}
+
+function toElementCommandeOuputApi(element: CommandeElement): ElementCommandeOutputApi {
+    return {
+        produit: toProduitOutputApi(element.produit),
+        quantite: element.quantité
+    }
+}
+
+function toProduitOutputApi(produit: Produit) {
+    return {
+        id: produit.id,
+        nom: produit.nom,
+        prix: produit.prix,
+        poids: produit.poids
+    }
+}
